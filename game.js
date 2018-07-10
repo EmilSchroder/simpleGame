36864ee9d9bb69be9515b3afcc66ba2f27747ba4
EmilSchroder <eschro66@gmail.com>
2018-07-10 14:25:40 +1200

    created a controlable paddle

--- game.js
+++ game.js
@@ -9,6 +9,13 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
 var ballRad = 10;
+//Defining Paddle and movement of paddle
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+var rightPressed = false;
+var leftPressed = false;
 
 //Defining movement
 var dx = 2;
@@ -23,6 +30,14 @@ function drawBall () {
     ctx.fill();
     ctx.closePath();
 
+}
+
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
 }
 
 function moveBall (){
@@ -40,16 +55,28 @@ if (x+dx-ballRad < 0 || x+dx+ballRad > canvas.width){
 }
 }
 
+function movePaddle (){
+        if (rightPressed && paddleX < canvas.width-paddleWidth){
+        paddleX += 7;
+    }
+    if (leftPressed && paddleX > 0){
+        paddleX -= 7;
+    }
+}
+
 
 //Epicentre
 function draw() {
     //Clears the canvas
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    //defines the ball and interactions
+    //defines the ball, paddle and interactions
     drawBall();
+    drawPaddle();
     collision();
+    movePaddle();
     moveBall();
-   
+
+
 
 }
 
@@ -59,4 +86,23 @@ function draw() {
 
 
 //executes "draw()" every 10ms
+document.addEventListener("keydown",keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+function keyDownHandler(evt){
+    if(evt.keyCode == 68){
+        rightPressed = true;
+    } else if (evt.keyCode == 65){
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(evt){
+    if (evt.keyCode == 68){
+        rightPressed = false;
+    } else if (evt.keyCode == 65){
+        leftPressed = false;
+    }
+}
+
 setInterval(draw,10);
