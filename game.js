7fa4baa8b96512504e04c9e6ec31992ac4e5cb74
EmilSchroder <eschro66@gmail.com>
2018-07-10 13:31:20 +1200

    Tidied code with embedded functions

--- game.js
+++ game.js
@@ -4,21 +4,6 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-function draw() {
-    //This draws the object that we want to move
-    ctx.beginPath();
-    ctx.arc(x,y,10,0,Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-    //update position of ball
-    x += dx;
-    y += dy;
-}
-
-//executes "draw()" every 10ms
-setInterval(draw,10);
-
 //sets the initial position of the ball
 var x = canvas.width/2;
 var y = canvas.height-30;
@@ -26,3 +11,33 @@ var y = canvas.height-30;
 //Defining movement
 var dx = 2;
 var dy = -2;
+
+
+function drawBall () {
+    //This draws the object that we want to move
+    ctx.beginPath();
+    ctx.arc(x,y,10,0,Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+}
+
+function moveBall (){
+    x += dx;
+    y += dy;
+}
+
+
+//Epicentre
+function draw() {
+    //Clears the canvas
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawBall();
+    moveBall();
+}
+
+
+
+//executes "draw()" every 10ms
+setInterval(draw,10);
